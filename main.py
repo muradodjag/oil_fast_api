d62573ca998328e834aca35192477ad5d92d0003
muradodjag <muradodjag@gmail.com>
2022-09-02 11:00:36 +0400

    add api key

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile,  Depends
+from fastapi.security.api_key import  APIKey
+import auth
 
 app = FastAPI()
 
 
-@app.post("/upload")
-def upload(file: UploadFile = File(...)):
+@app.post("/api/v1/upload")
+async def upload(
+            file: UploadFile = File(...), api_key: APIKey = Depends(auth.get_api_key)):
     try:
         contents = file.file.read()
         with open(file.filename, 'wb') as f:
--- test.py
+++ test.py
@@ -2,9 +2,11 @@ import requests
 import json
 
 multipart_form_data = {
-    'file': ('2.jpg', open('1.jpg', 'rb')),
+    'file': ('3.jpg', open('test.jpg', 'rb')),
 }
+headers = {
+    'access_token' : '524c2a8d1805dba6707039618707780b'
+}
+response = requests.post('http://127.0.0.1:8000/api/v1/upload', files=multipart_form_data, headers=headers)
 
-response = requests.post('http://127.0.0.1:8000/upload', files=multipart_form_data)
-
-print(json.loads(response.content))
+print(response.status_code,json.loads(response.content))
